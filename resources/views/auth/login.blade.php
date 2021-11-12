8ca87440a11034fc0e03bc26fe7b95783c75394b
ArticBurn <ivanandika94@gmail.com>
2021-11-12 22:12:59 +0700

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,74 +1,69 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Login Page</title>
+    <title>Admin Login - {{ Fungsi::app_nama() }}</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='{{asset('/')}}assets/css/kopekstylesheet.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='{{ asset('/') }}assets/css/kopekstylesheet.css'>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;1,300;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;1,300;1,400&display=swap"
+        rel="stylesheet">
     <!-- <script src='main.js'></script> -->
     <script src="https://unpkg.com/boxicons@2.0.9/dist/boxicons.js"></script>
 </head>
+
 <body>
     <div id="container">
         <div id="wrapper">
-            <form action="{{ route('login') }}" method="POST">
-                @csrf
             <div id="loginForm">
-                <h1>Login</h1>
-                <span>Sign in to your account</span>
-                <div id="inputField">
-                    <input type="text" placeholder="Username" autocomplete="nope" name="identity" >
-                </div>
+                <form action="{{ route('login') }}" method="POST" >
+                    @csrf
 
-  @if($errors->any())
-  @foreach ($errors->all() as $error)
-  <p id="wrong" class="wrong">
-    {{ $error }}
-  </p>
-  @endforeach
-@endif
-                <div id="inputField">
-                    <input type="password" placeholder="Password" autocomplete="nope"  name="password">
-                </div>
+                    <h1>Login</h1>
+                    <div id="inputField">
+                        <input type="text" placeholder="Username" autocomplete="nope" name="identity">
+                    </div>
+                    @error('identity')
+                        <p id="wrong" class="wrong">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                    <div id="inputField">
+                        <input type="password" placeholder="Password" autocomplete="nope" name="password">
+                    </div>
 
-                @error('password')
-                <p id="wrong" class="wrong">
-                    {{ $message }}
-                </p>
-            @enderror
+                    @error('password')
+                        <p id="wrong" class="wrong">
+                            {{ $message }}
+                        </p>
+                    @enderror
 
-                <div id="inputSignIn">
-                    <input type="submit" value="Login">
-                </div>
-                <p>Not registered? <a href="#">Create an account</a></p>
+                    <div id="inputSignIn">
+                        <input type="submit" value="Login">
+                    </div>
+                </form>
             </div>
-            </form>
             <div id="logoCompany">
                 <div id="opacity">
                     <a href="#" target="_blank" id="linkLogo">
-                        <img src="assets/github.png" alt="Your Logo">
+                        <img src='{{asset('/assets/img/kopek/cosmetics.png')}}' alt="Your Logo">
                     </a>
                     <p>
-                        Ramdani Skincare
+                        {{ Fungsi::app_nama() }}
                     </p>
                 </div>
             </div>
         </div>
-        {{-- <div id="credit">
+        <div id="credit">
             <p>
-                Designed with
-                <box-icon name='heart' type='solid' color='#ff1014' size="xs"></box-icon>
-                by <a href="https://baemon.web.id/" target="_blank">Baemon Team</a>
+                Demo : user = admin // pass = admin
             </p>
         </div>
-        <div id="icon">
-            <box-icon name='github' type='logo' animation='tada-hover' size='md'></box-icon>
-        </div> --}}
     </div>
 </body>
+
 </html>
