685999d827c7f9ade20df1d3f3a39bceaf8e7407
godgodwinter <kakadz.pro@gmail.com>
2021-11-15 21:57:41 +0700

    update layouit

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -43,13 +43,13 @@
             <img alt="image" src="{{ asset('/') }}assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
             <div class="d-sm-none d-lg-inline-block">{{ Auth::user()->name }}</div></a>
             <div class="dropdown-menu dropdown-menu-right">
-              <a href="#" class="dropdown-item has-icon">
-                <i class="far fa-user"></i> Profile
+              <a href="{{url('/')}}" class="dropdown-item has-icon">
+                <i class="fas fa-house-damage"></i> Beranda Luar
               </a>
 
-              <a href="#" class="dropdown-item has-icon">
+              {{-- <a href="#" class="dropdown-item has-icon">
                 <i class="fas fa-cog"></i> Settings
-              </a>
+              </a> --}}
               <div class="dropdown-divider"></div>
               <form method="POST" action="{{ route('logout') }}">
                 @csrf
